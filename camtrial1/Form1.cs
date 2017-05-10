9f756a22e6592af4dd959f628fe116ca4d566df0
CaptainSundae <nikisiasoco@gmail.com>
2017-05-11 00:45:39 +1200

    Add project files.

---
+++ camtrial1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+namespace camtrial1
+{
+    public partial class Form1 : Form
+    {
+        //initialize components, the webcam: Is for info collection
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection webcam;
+        private VideoCaptureDevice camera;
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in webcam)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);      
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            camera = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString);
+            camera.NewFrame += new NewFrameEventHandler(cameraNewFrame);
+            camera.Start();
+        }
+
+        void cameraNewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bit;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (camera.IsRunning)
+            {
+                camera.Stop();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = @"D:\picture";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+    }
+}
